73c32a653b13eb892cdc6494cb584c4ab8a9b45d
adMenon <adityaharidas.m16@iiits.in>
2018-10-24 12:02:14 +0530

    bug fix

--- routes/catalogue.js
+++ routes/catalogue.js
@@ -1,16 +1,23 @@
 var catalogue = function(app,urlencodedParser,con){
 
 	app.post("/catalogue",urlencodedParser,function(req,res){
-		var q = "select * from BookDetail";
-		con.query(q,function(err,resp){
-			if(err)
-				throw err;
-			output = JSON.parse(JSON.stringify(resp));
-			if(output.length == 0)
-				res.send("No books found");
-			else
-				res.send({"book-details":output});
-		});
+		if(req.session.username && req.cookies.user_sid)
+		{
+			var q = "select * from BookDetail";
+			con.query(q,function(err,resp){
+				if(err)
+					throw err;
+				output = JSON.parse(JSON.stringify(resp));
+				if(output.length == 0)
+					res.send("No books found");
+				else
+					res.send({"book-details":output});
+			});
+		}
+		else
+		{
+			res.send({"output":"notloggedin"});
+		}
 	});
 }
 

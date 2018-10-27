25c41b3c9b1cdaf90061f0b76b5a41f0108c4cbd
urstrulyrohit <risingrohit18@gmail.com>
2018-10-28 01:13:29 +0530

    initialized sidenav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { SideNav, Nav } from 'react-sidenav'
 import Header from './Header';
 import Footer from './Footer';
 import './App.css';
@@ -7,6 +8,11 @@ class App extends Component {
   render() {
     return (
       <div>
+        <SideNav defaultSelectedPath="1">
+            <Nav id="1">Item 1</Nav>
+            <Nav id="2">Item 2</Nav>
+            <Nav id="3">Item 3</Nav>
+        </SideNav>
         <Header />
         <Footer />
       </div>

dc948fc1b449a1346d93849bcaacafe4bbaa4f91
urstrulyrohit <risingrohit18@gmail.com>
2018-10-28 01:05:24 +0530

    added header footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Header from './Header';
+import Footer from './Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
+        <Footer />
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {PageHeader} from 'react-bootstrap'
+import './Header.css'
+
+const Header = () => {
+
+
+	return (
+		<div className='bg-success'>
+			<PageHeader>
+			  My admin
+			</PageHeader>
+		</div>
+	);
+}
+
+export default Header;

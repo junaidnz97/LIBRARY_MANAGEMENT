74b9e69109b06d0e3afdd9304705fc50081a599f
parasdahiya <paras.d16@iiits.in>
2018-10-30 03:15:37 +0530

    Removed problematic footer

--- frontend/library/src/Footer.js
+++ frontend/library/src/Footer.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import {PageHeader} from 'react-bootstrap'
-
 import './Footer.css'
-
-
 const Footer = (props) => {
 
 	return (
-		<div className='page-footer'>
-				<div className='bg-success' >
-		<PageHeader style={{ marginTop: "10px"}}>
-			Library@IIITS<br /><small>720 GnanMarg,SriCity</small>
-			
-		</PageHeader>
-	</div>
-	</div>
-	
+		<div>
+			<div className='bg-success'>
+				<PageHeader>
+					Library@IIITS<br /><small>720 GnanMarg,SriCity</small>	
+				</PageHeader>
+			</div>
+		</div>
 	);
 }
 

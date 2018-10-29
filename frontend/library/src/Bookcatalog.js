9028b3fadc90516a20371f19356077724b664066
parasdahiya <paras.d16@iiits.in>
2018-10-30 02:10:08 +0530

    Fixing display probs

--- frontend/library/src/Bookcatalog.js
+++ frontend/library/src/Bookcatalog.js
@@ -5,6 +5,7 @@ import * as axios from 'axios';
 import Header from './Header.js'
 import Listcontainer from './Listcontainer.js'
 import Cardlist from './Cardlist.js';
+import {Pagination} from 'react-bootstrap';
 //import Searchbox from './Searchbox,js';
 import {robots} from './robots.js';
 
@@ -13,6 +14,7 @@ class Bookcatalog extends Component {
         super();
         this.state = {
             books : [],
+            c : 1
             //searchfield = ''
         }
     }
@@ -49,6 +51,43 @@ class Bookcatalog extends Component {
                 <Listcontainer>
                 <Container fluid>
                     <Cardlist books = {this.state.books} />
+                    <center>
+                        <Pagination>
+                            
+
+                        {(() => {
+                            switch(this.state.c) {
+                                case 1:
+                                    return <React.Fragment><Pagination.Prev disabled/>
+                                    <Pagination.Item>{this.state.c}</Pagination.Item>
+                                    <Pagination.Ellipsis />
+                                    <Pagination.Item>{6}</Pagination.Item>
+                                    <Pagination.Next/>
+                                    <Pagination.Last/>
+                                    </React.Fragment>
+                                case 6:
+                                    return <React.Fragment><Pagination.Item>{1}</Pagination.Item>
+                                    <Pagination.Ellipsis />
+                                    <Pagination.Item>{5}</Pagination.Item>
+                                    <Pagination.Item>{6}</Pagination.Item>
+                                    <Pagination.Item disabled>{7}</Pagination.Item></React.Fragment>
+                                default:
+                                    return <React.Fragment>
+                                        <Pagination.First/>    
+                                        <Pagination.Prev/>
+                                        <Pagination.Ellipsis />
+                                        <Pagination.Item>{this.state.c-1}</Pagination.Item>
+                                        <Pagination.Item>{this.state.c}</Pagination.Item>
+                                        <Pagination.Item>{this.state.c+1}</Pagination.Item>
+                                        <Pagination.Ellipsis />
+                                        <Pagination.Next/>
+                                        <Pagination.Last/>
+                                    </React.Fragment>
+                                }
+                        })()}
+ 
+                        </Pagination>
+                        </center>
                 </Container>
                 </Listcontainer>
             </div>

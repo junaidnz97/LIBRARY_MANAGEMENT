26ca03673b273adece019a64dc10a6981b8ced9e
parasdahiya <paras.d16@iiits.in>
2018-10-31 01:24:16 +0530

    Login page enhanced

--- frontend/library/src/index.js
+++ frontend/library/src/index.js
@@ -7,14 +7,15 @@ import 'tachyons';
 //import Payfine from './Payfine.js'
 import Admin from './Admin.js'
 import App from './App.js';
-import Book from './Book.js'
+import Book from './Book.js';
+import Login from './Login.js';
 import { BrowserRouter } from 'react-router-dom'
 
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
 	<BrowserRouter>
-		<App/>
+		<Login/>
 	</BrowserRouter>
 ), document.getElementById('root'));
 registerServiceWorker();

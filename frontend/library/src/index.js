8cf9cbb8225687f66da7c38bb56fa65f17dfd3d4
parasdahiya <paras.d16@iiits.in>
2018-10-31 00:07:24 +0530

    Cleaning code

--- frontend/library/src/index.js
+++ frontend/library/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
-//import Profile from './Profile.js'
-//import Suggest from './Suggest.js'
-//import Donate from './Donate.js'
-//import Payfine from './Payfine.js'
 import Admin from './Admin.js'
 import App from './App.js';
 import Book from './Book.js'
